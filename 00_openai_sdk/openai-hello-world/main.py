434fa1d0b8441ce76c80c5fe99be5a46b40362f6
Mohsin-ali897 <mohsinlashari897915@gmail.com>
2025-05-06 14:44:02 +0500

    open ai agent sdk

---
+++ 00_openai_sdk/openai-hello-world/main.py
@@ -0,0 +1,44 @@
+import chainlit as cl # type: ignore
+
+import os
+from agents import Agent, Runner, AsyncOpenAI, RunConfig, OpenAIChatCompletionsModel # type:ignore
+from dotenv import load_dotenv, find_dotenv # type: ignore
+#! step 1: load the environment variables 
+load_dotenv(find_dotenv())
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+#? step 2: setting up the external client  
+provider = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url='https://generativelanguage.googleapis.com/v1beta/openai/'
+)
+#? step 3: setting up the Chat Completions Model
+model = OpenAIChatCompletionsModel(
+    model = 'gemini-2.0-flash', 
+    openai_client = provider,
+)
+#? step 4: setting up the configuration for the agent 
+run_config = RunConfig(
+    model = model,
+    model_provider = provider,
+    tracing_disabled = True,
+)
+
+#? step 5: setting up the agent
+agent:Agent = Agent(
+    name='Assistant',
+    instructions='A helpful assistant',
+    model = model,
+)
+# Result = Runner.run_sync(agent, 'what is the capital of France?', run_config=run_config)
+# print(Result.final_output)
+@cl.on_message
+async def handle_message(message: cl.Message):
+    """
+    Handle the message from the user.
+    """
+    Result = await Runner.run(agent, message.content, run_config=run_config)
+    await cl.Message(content=Result.final_output).send()
+    
+
+    
+
